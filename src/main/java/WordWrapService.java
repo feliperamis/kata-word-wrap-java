0abb4ea73aae416227c1f6005b290a29687faf53
fjramis <fjramis@leadtech.com>
2021-04-29 18:02:40 +0200

    Init

---
+++ src/main/java/WordWrapService.java
@@ -0,0 +1,11 @@
+
+
+public class WordWrapService {
+    WordWrapService() {
+      // An useless constructor
+    }
+
+    public String helloWorld() {
+        return "Hello world - Word wrap";
+    }
+}
---
+++ src/test/java/WordWrapServiceTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class WordWrapServiceTest {
+    private static WordWrapService wordWrapService;
+
+    @BeforeAll
+    public static void setup() {
+        wordWrapService = new WordWrapService();
+    }
+
+
+    @Test
+    public void returnHelloWorld() {
+        String helloMessage = wordWrapService.helloWorld();
+        Assertions.assertEquals(helloMessage, "Hello world - Word wrap");
+    }
+
+}
